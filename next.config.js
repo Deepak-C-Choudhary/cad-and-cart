1742b3168fa08e3e6a88af1aca675d06044ccf57
Deepak Choudhary <163335534+Deepak-C-Choudhary@users.noreply.github.com>
2024-12-18 15:14:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ["dogapi.dog"], // Ensure image domains are whitelisted
+    domains: ["dogapi.dog"], 
   },
 };
 
